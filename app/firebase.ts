9687b9783456ddb9d0e1992f24e7931d00116252
rbs <robis24@gmail.com>
2025-04-07 21:32:40 +0200

    Update firebase.ts

--- app/firebase.ts
+++ app/firebase.ts
@@ -17,7 +17,7 @@ const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: "energiehulp.firebasestorage.app",
+  storageBucket: import.meta.env.VITE_FIREBASE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
